57fde5aefcebc64d97d5c8e8a212c2b15b87f342
Hallie-Johnson <HEJohnson130@gmail.com>
2024-12-04 10:19:55 -0500

    Deployment Fix 4

--- backend/server.js
+++ backend/server.js
@@ -122,6 +122,6 @@ app.get('/api/reports-data', async (req, res) => {
 
 
 // Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
